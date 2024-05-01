d52669faa49a797f18a8409355a48c189884bf5a
Dmitriy Tikhomirov <uroborosq@ya.ru>
2024-05-01 22:14:17 +0300

    some mess

--- internal/list/list.go
+++ internal/list/list.go
@@ -9,7 +9,7 @@ import (
 
 type repo interface {
 	ID(context.Context, uuid.UUID) (ProductListResponse, error)
-	Create(context.Context, ProductListResponse) (ProductListResponse, error)
+	Create(context.Context, ProductListResponse) error
 	UserID(context.Context, uuid.UUID) ([]ProductListResponse, error)
 	Update(context.Context, ProductListResponse) (ProductListResponse, error)
 }
@@ -22,7 +22,7 @@ func NewService(repo repo) *Service {
 	return &Service{repo: repo}
 }
 
-func (s *Service) Create(name string, creatorID uuid.UUID) (ProductListResponse, error) {
+func (s *Service) Create(ctx context.Context, name string, creatorID uuid.UUID) (ProductListResponse, error) {
 	list := ProductListResponse{
 		ProductListRequest: ProductListRequest{
 			ID:     uuid.New(),
@@ -33,11 +33,18 @@ func (s *Service) Create(name string, creatorID uuid.UUID) (ProductListResponse,
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
 	}
-
+	return list, s.repo.Create(ctx, list)
 }
 
-func (s *Service) Update(list ProductListResponse) (ProductListResponse, error) {
-	panic("Not implemented")
+func (s *Service) Update(list ProductListRequest) (ProductListResponse, error) {
+	model := ProductListResponse{
+		ProductListRequest: list,
+		OwnerID:            uuid.UUID{},
+		ViewerIDList:       nil,
+		CreatedAt:          time.Time{},
+		UpdatedAt:          time.Now(),
+	}
+	return
 }
 
 func (s *Service) Delete(id uuid.UUID) (ProductListResponse, error) {
